0a5c4975a5496323a2d008bebde1e444d227375f
kevin lang <langkevina@gmail.com>
2019-06-05 23:48:40 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -26,9 +26,8 @@ class Song
   end
 
   def self.find_by_name(name)
-      self.all.find{|song| song.name == name}
-    end
-
+    self.all.find{|song| song.name == name}
+  end
 
   def save
     self.class.all << self
