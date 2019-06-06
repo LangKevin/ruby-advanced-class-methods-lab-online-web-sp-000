1eb866def9f7c26878b82a0b891e85b46654ed3d
kevin lang <langkevina@gmail.com>
2019-06-06 00:06:40 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -16,12 +16,12 @@ class Song
 
   def self.new_by_name(name)
     song = self.new
-    song.name = name
+    @name = name
   end
 
   def self.create_by_name(name)
     song = self.new
-    song.name = name
+    @name = name
     @@all << song
   end
 
