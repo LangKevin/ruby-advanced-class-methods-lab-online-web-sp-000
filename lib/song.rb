5fa373874d788a902f4afdbfe4d759e841f841a1
kevin lang <langkevina@gmail.com>
2019-06-06 00:53:17 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -56,6 +56,7 @@ class Song
   #   it 'initializes a song and artist_name based on the filename format' do
   #     song = Song.new_from_filename("Thundercat - For Love I Come.mp3")
   def self.new_from_filename(file)
+    locFile = file.remove(".mp3")
     art_name, name = file.split(" - ")
     song = self.new
     song.name = name
