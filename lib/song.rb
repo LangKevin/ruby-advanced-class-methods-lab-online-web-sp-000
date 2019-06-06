13877cf8cfce31630d3cf16c3f39a8200c358fa6
kevin lang <langkevina@gmail.com>
2019-06-06 00:54:50 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -57,7 +57,7 @@ class Song
   #     song = Song.new_from_filename("Thundercat - For Love I Come.mp3")
   def self.new_from_filename(file)
     locFile = file
-    locFile..remove(".mp3")
+    locFile.remove(".mp3")
     art_name, name = locFile.split(" - ")
     song = self.new
     song.name = name
